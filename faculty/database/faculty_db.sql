9b04a4ee876ac06caee8a3913c57fb6f19ae50e4
IgorYevtushenkoUA <igor.yevtushenko.ua@gmail.com>
2021-09-29 11:21:38 +0300

    update database info

--- faculty/database/faculty_db.sql
+++ faculty/database/faculty_db.sql
@@ -12,27 +12,27 @@ alter table role
 create unique index role_id_uindex
     on role (id);
 
-create table "user"
+create table users
 (
-    id          serial
+    id          bigint default nextval('user_id_seq'::regclass) not null
         constraint user_pk
             primary key,
-    first_name  varchar(32)  not null,
-    second_name varchar(32)  not null,
-    last_name   varchar(32)  not null,
-    email       varchar(128) not null,
-    password    varchar(32)  not null,
+    first_name  varchar(32)                                     not null,
+    second_name varchar(32)                                     not null,
+    last_name   varchar(32)                                     not null,
+    email       varchar(128)                                    not null,
+    password    varchar(512)                                    not null,
     role_id     integer
         constraint user_role_id_fk
             references role
             on update cascade on delete cascade
 );
 
-alter table "user"
+alter table users
     owner to postgres;
 
 create unique index user_id_uindex
-    on "user" (id);
+    on users (id);
 
 create table status
 (
@@ -50,11 +50,11 @@ create unique index status_id_uindex
 
 create table student
 (
-    id         integer not null
+    id         bigint  not null
         constraint student_pk
             primary key
         constraint student_user_id_fk
-            references "user"
+            references users
             on update cascade on delete cascade,
     course_num integer not null,
     enable     boolean not null
@@ -65,12 +65,11 @@ alter table student
 
 create table teacher
 (
-    id integer not null
+    id bigint not null
         constraint teacher_pk
             primary key
         constraint teacher_user_id_fk
-            references "user"
-            on update cascade on delete cascade
+            references users
 );
 
 alter table teacher
@@ -95,12 +94,14 @@ create table course
     id                serial
         constraint course_pk
             primary key,
-    topic_id          integer       not null,
+    topic_id          integer       not null
+        constraint fkokaxyfpv8p583w8yspapfb2ar
+            references topic,
     capacity          integer       not null,
     semester_start    integer       not null,
     semester_duration integer       not null,
     description       varchar(1024) not null,
-    teacher_id        integer       not null
+    teacher_id        integer
         constraint course_teacher_id_fk
             references teacher
             on update cascade on delete cascade,
@@ -115,19 +116,20 @@ create unique index course_id_uindex
 
 create table student_has_course
 (
-    student_id integer not null
+    student_id     integer   not null
         constraint student_has_course_student_id_fk
             references student
             on update cascade on delete cascade,
-    course_id  integer not null
+    course_id      integer   not null
         constraint student_has_course_course_id_fk
             references course
             on update cascade on delete cascade,
-    status_id  integer
+    status_id      integer
         constraint student_has_course_status_id_fk
             references status
             on update cascade on delete cascade,
-    mark       integer,
+    mark           integer,
+    recording_time timestamp not null,
     constraint student_has_course_pk
         primary key (course_id, student_id)
 );
